08310ac4266c5285e1e2165e3597106c6938a154
Tre`Von McKay <trevon.mckay@gmail.com>
2017-01-04 15:49:06 -0800

    Code cleanup

--- src/Spotify.NET.Client/SpotifyClient.cs
+++ src/Spotify.NET.Client/SpotifyClient.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-//using System.Extensions;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
